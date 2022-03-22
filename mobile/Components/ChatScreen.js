f9ac55f92a3b9801233f8f3fd49fb4030d050d27
Julian Cavanagh <juliancavanagh@hotmail.com>
2022-03-22 17:18:26 +0000

    create chatscreen component

---
+++ mobile/Components/ChatScreen.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import { Text, TextInput, StyleSheet, View, Button } from "react-native";
+// import Message from "./Message";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:4000");
+
+export default function ChatScreen() {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [chat, setChat] = useState([]);
+
+  useEffect(() => {
+    function messageListener(message) {
+      setChat((currChat) => [...currChat, message]);
+    }
+    socket.on("chat message", messageListener);
+    return () => {
+      socket.off("chat message", messageListener);
+    };
+  }, [socket]);
+
+  function handleSubmit() {
+    console.log(currentMessage);
+    socket.emit("chat message", currentMessage);
+  }
+
+  return (
+    <View style={styles.container}>
+      {/* <Message message={message} /> */}
+      <View style={styles.messageField}>
+        <TextInput
+          placeholder="Enter Your Message Here..."
+          placeholderTextColor={"black"}
+          value={currentMessage}
+          onChangeText={setCurrentMessage}
+        ></TextInput>
+        <Button title="submit" onPress={handleSubmit} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  messageField: {
+    alignItems: "center",
+    textAlign: "center",
+    position: "absolute",
+    bottom: 0,
+  },
+});
