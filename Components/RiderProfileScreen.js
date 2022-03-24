d3aceef8f239b9fe53256ec880803abad9e05db0
MrChris-H <christopher.hopkins998@gmail.com>
2022-03-24 21:13:13 +0000

    add basic css for rider profile

--- Components/RiderProfileScreen.js
+++ Components/RiderProfileScreen.js
@@ -1,41 +1,116 @@
 import React from "react";
-import { Text, View, Button, Image, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
 
 export default function RiderProfileScreen({ route }) {
+  const { height, width } = Dimensions.get("window");
   const { rider, profileUrl } = route.params;
   console.log(rider, profileUrl);
   return (
     <View style={styles.container}>
-      <Image
-        style={styles.logo}
-        source={{
-          uri: profileUrl,
-        }}
-      />
-      <Text style={styles.name}>{`${rider.firstName} ${rider.lastName}`}</Text>
-      <Text>405 followers</Text>
-      <Text>{rider.age}</Text>
-      <Text>{rider.bio}</Text>
-      <Text>{rider.bike}</Text>
-      <Text>{rider.gender}</Text>
-      <Text>1 mile</Text>
-      <Button title="Message" color="black" onPress={() => {}} />
+      <View style={styles.imgContainer}>
+        <Image
+          style={styles.logo}
+          source={{
+            uri: profileUrl,
+          }}
+        />
+      </View>
+      <View style={styles.info}>
+        <View style={styles.infoTop}>
+          <Text
+            style={styles.name}
+          >{`${rider.firstName} ${rider.lastName}`}</Text>
+          {/* <Text>405 followers</Text> */}
+          <Text style={styles.age}>{rider.age}</Text>
+        </View>
+
+        <Text style={styles.bio}>{rider.bio}</Text>
+        <Text style={styles.bike}>{rider.bike}</Text>
+        <View style={styles.infoLower}>
+          <Text style={styles.gender}>{rider.selectedGender}</Text>
+          <Text style={styles.distance}>1 mile</Text>
+        </View>
+      </View>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.text}>Message</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const { height, width } = Dimensions.get("window");
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 50,
-    alignItems: "center",
+    paddingTop: 20,
+  },
+  imgContainer: {
+    justifyContent: "center",
+    flexDirection: "row",
   },
   logo: {
     justifyContent: "center",
-    width: 300,
-    height: 300,
+    width: width * 0.95,
+    height: width * 0.95,
+  },
+  info: {
+    flex: 1,
+    paddingHorizontal: width * 0.025,
+  },
+  infoTop: {
+    flexDirection: "row",
   },
   name: {
-    fontSize: 50,
+    fontSize: 40,
     textAlign: "left",
+    flex: 4,
+  },
+  age: {
+    fontSize: 40,
+    textAlign: "right",
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+    flex: 1,
+  },
+  bio: {
+    flex: 1,
+    fontSize: 22,
+  },
+  bike: {
+    fontSize: 18,
+  },
+  infoLower: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  gender: {
+    fontSize: 18,
+  },
+  distance: {
+    fontSize: 18,
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 13,
+    paddingHorizontal: 32,
+    elevation: 3,
+    backgroundColor: "black",
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
   },
 });
