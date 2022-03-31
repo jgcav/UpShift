5f8a511dc1285a748748227491d8bfb2ab9925c6
MrChris-H <christopher.hopkins998@gmail.com>
2022-03-31 03:15:22 +0100

    add background image to login

--- Components/LoginScreen.js
+++ Components/LoginScreen.js
@@ -2,12 +2,13 @@ import React, { useRef, useState } from "react";
 import {
   StyleSheet,
   View,
-  Text,
   TouchableOpacity,
   StatusBar,
-  Button,
+  ImageBackground,
+  Dimensions,
 } from "react-native";
 import { useAuth } from "../contexts/AuthContext";
+import { Text, Card, Icon, Button } from "@rneui/base";
 import LoginLogo from "./LoginLogo";
 import TextInput from "./TextInput.js";
 
@@ -36,36 +37,56 @@ export default function LoginScreen({ navigation: { navigate } }) {
         else setError("Failed to Sign in");
       });
   }
+  const { height, width } = Dimensions.get("window");
 
   return (
-    <View behavior="padding" style={style.container}>
-      <StatusBar barStyle="light-content" />
-      <LoginLogo />
-      <TextInput
-        style={style.input}
-        placeholder="Email address"
-        placeholderTextColor={"#a9a9a9"}
-        returnKeyType="next"
-        keyboardType="email-address"
-        autoCapitalize="none"
-        value={email}
-        onChangeText={setEmail}
-        autoCorrect={false}
-      />
-      <TextInput
-        style={style.input}
-        placeholder="Password"
-        placeholderTextColor={"#a9a9a9"}
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <Text>{error && error}</Text>
-      <TouchableOpacity style={style.buttonContainer}>
-        <Button title="Login" color="white" onPress={handleSubmit} />
-      </TouchableOpacity>
-      <Text>
-        Don't have an account?{"  "}
+    <ImageBackground
+      source={require("../assets/logInPhoto.jpg")}
+      style={{ width: width, height: height }}
+    >
+      <View behavior="padding" style={style.container}>
+        <StatusBar barStyle="light-content" />
+        <LoginLogo />
+        <TextInput
+          style={style.input}
+          placeholder="Email address"
+          placeholderTextColor={"#a9a9a9"}
+          returnKeyType="next"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          value={email}
+          onChangeText={setEmail}
+          autoCorrect={false}
+        />
+        <TextInput
+          style={style.input}
+          placeholder="Password"
+          placeholderTextColor={"#a9a9a9"}
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        <Text>{error && error}</Text>
+        <Button
+          title="Login"
+          buttonStyle={{
+            borderRadius: 0,
+            width: 200,
+            height: 35,
+            padding: 0,
+            backgroundColor: "#7379C6",
+          }}
+          containerStyle={{
+            borderRadius: 0,
+            width: 202,
+            height: 37,
+            padding: 0,
+            borderWidth: 1,
+          }}
+          onPress={handleSubmit}
+        />
+        <Text>
+          Don't have an account?{"  "}
           <Text
             style={style.textHighlight}
             onPress={() => {
@@ -74,8 +95,9 @@ export default function LoginScreen({ navigation: { navigate } }) {
           >
             Sign Up
           </Text>
-      </Text>
-    </View>
+        </Text>
+      </View>
+    </ImageBackground>
   );
 }
 const style = StyleSheet.create({
@@ -83,16 +105,14 @@ const style = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#FFFF",
   },
   input: {
-    height: 40,
     width: 300,
     backgroundColor: "#FFFF",
-    marginBottom: 20,
+    margin: 0,
+    padding: 0,
     color: "black",
-    paddingHorizontal: 10,
-    fontSize: 20,
+    fontSize: 16,
   },
   buttonContainer: {
     backgroundColor: "#CFDDF6",
